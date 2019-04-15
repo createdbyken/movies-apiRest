79657be6ff05ac3988020e39d26592373160a761
mau5ken <harygato3@gmail.com>
2019-04-15 05:21:02 -0500

    Added nodelmon and configured a port for heroku and aws

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const app = express();
 const morgan = require('morgan');
 
 //settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
 
 // starting the server
 app.listen(app.get('port'), () => {
